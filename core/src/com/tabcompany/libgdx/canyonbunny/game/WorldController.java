5aa8ea7d56e959bafcd9a0d05f3f954434c06cb3
tabcompany <info.tabcompany@gmail.com>
2017-12-28 23:11:27 +0330

    Building the game loop

---
+++ core/src/com/tabcompany/libgdx/canyonbunny/game/WorldController.java
@@ -0,0 +1,17 @@
+package com.tabcompany.libgdx.canyonbunny.game;
+
+public class WorldController {
+
+    private static final String TAG = WorldController.class.getName();
+
+    public WorldController () {
+
+    }
+    private void init () {
+
+    }
+
+    public void update (float deltaTime) {
+
+    }
+}
---
+++ desktop/src/com/tabcompany/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.tabcompany.libgdx.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tabcompany.libgdx.canyonbunny.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
