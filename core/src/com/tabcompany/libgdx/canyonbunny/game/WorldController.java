d2afa0d85f5cdef007b34d66d377fa75561f66a2
tabcompany <info.tabcompany@gmail.com>
2018-01-02 03:13:56 +0330

    Adding collision detection and controling the player character

--- core/src/com/tabcompany/libgdx/canyonbunny/game/WorldController.java
+++ core/src/com/tabcompany/libgdx/canyonbunny/game/WorldController.java
@@ -2,15 +2,15 @@ package com.tabcompany.libgdx.canyonbunny.game;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.math.Rectangle;
+import com.tabcompany.libgdx.canyonbunny.game.objects.BunnyHead;
+import com.tabcompany.libgdx.canyonbunny.game.objects.Feather;
+import com.tabcompany.libgdx.canyonbunny.game.objects.GoldCoin;
+import com.tabcompany.libgdx.canyonbunny.game.objects.Rock;
 import com.tabcompany.libgdx.canyonbunny.util.CameraHelper;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.Array;
+
 import com.tabcompany.libgdx.canyonbunny.util.Constants;
 
 public class WorldController extends InputAdapter {
@@ -18,6 +18,10 @@ public class WorldController extends InputAdapter {
     private static final String TAG = WorldController.class.getName();
 
     public CameraHelper cameraHelper;
+
+    // Rectangles for collision detection
+    private Rectangle r1 = new Rectangle();
+    private Rectangle r2 = new Rectangle();
 
     /*for test*/
 //    public Sprite[] testSprites;
@@ -40,35 +44,9 @@ public class WorldController extends InputAdapter {
         initLevel();
     }
 
-    private void initLevel () {
+    private void initLevel() {
         score = 0;
         level = new Level(Constants.LEVEL_01);
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        // Reset game world
-        if (keycode == Keys.R) {
-            init();
-            Gdx.app.debug(TAG, "Game world resetted");
-        }
-
-        /*for test*/
-        // Select next sprite
-//        else if (keycode == Keys.SPACE) {
-//            selectedSprite = (selectedSprite + 1) % testSprites.length;
-//            // Update camera's target to follow the currently selected sprite
-//            if (cameraHelper.hasTarget()) {
-//                cameraHelper.setTarget(testSprites[selectedSprite]);
-//            }
-//            Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
-//        }
-        // Toggle camera follow
-//        else if (keycode == Keys.ENTER) {
-//            cameraHelper.setTarget(cameraHelper.hasTarget() ? null : testSprites[selectedSprite]);
-//            Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
-//        }
-        return false;
     }
 
     /*for test*/
@@ -125,23 +103,96 @@ public class WorldController extends InputAdapter {
 ////        selectedSprite = 0;
 //    }
 
-    private Pixmap createProceduralPixmap(int width, int height) {
-        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
-        // Fill square with red color at 50% opacity
-        pixmap.setColor(1, 0, 0, 0.5f);
-        pixmap.drawLine(0, 0, width, height);
-        pixmap.drawLine(width, 0, 0, height);
-        // Draw a cyan-colored border around square
-        pixmap.setColor(0, 1, 1, 1);
-        pixmap.drawRectangle(0, 0, width, height);
-        return pixmap;
-    }
+//    private Pixmap createProceduralPixmap(int width, int height) {
+//        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+//        // Fill square with red color at 50% opacity
+//        pixmap.setColor(1, 0, 0, 0.5f);
+//        pixmap.drawLine(0, 0, width, height);
+//        pixmap.drawLine(width, 0, 0, height);
+//        // Draw a cyan-colored border around square
+//        pixmap.setColor(0, 1, 1, 1);
+//        pixmap.drawRectangle(0, 0, width, height);
+//        return pixmap;
+//    }
 
     public void update(float deltaTime) {
         handleDebugInput(deltaTime);
+        handleInputGame(deltaTime);
         //updateTestObjects(deltaTime);
         level.update(deltaTime);
+        testCollisions();
         cameraHelper.update(deltaTime);
+    }
+
+    private void testCollisions() {
+        r1.set(level.bunnyHead.position.x, level.bunnyHead.position.y,
+                level.bunnyHead.bounds.width, level.bunnyHead.bounds.height);
+
+        // Test collision: Bunny Head <-> Rocks
+        for (Rock rock : level.rocks) {
+            r2.set(rock.position.x, rock.position.y, rock.bounds.width, rock.bounds.height);
+            if (!r1.overlaps(r2)) continue;
+            onCollisionBunnyHeadWithRock(rock);
+            // IMPORTANT: must do all collisions for valid edge testing on rocks.
+        }
+
+        // Test collision: Bunny Head <-> Gold Coins
+        for (GoldCoin goldCoin : level.goldcoins) {
+            if (goldCoin.collected) continue;
+            r2.set(goldCoin.position.x, goldCoin.position.y, goldCoin.bounds.width, goldCoin.bounds.height);
+            if (!r1.overlaps(r2)) continue;
+            onCollisionBunnyWithGoldCoin(goldCoin);
+            break;
+        }
+
+        // Test collision: Bunny Head <-> Feathers
+        for (Feather feather : level.feathers) {
+            if (feather.collected) continue;
+            r2.set(feather.position.x, feather.position.y, feather.bounds.width, feather.bounds.height);
+            if (!r1.overlaps(r2)) continue;
+            onCollisionBunnyWithFeather(feather);
+            break;
+        }
+    }
+
+    private void onCollisionBunnyHeadWithRock(Rock rock) {
+        BunnyHead bunnyHead = level.bunnyHead;
+        float heightDifference = Math.abs(bunnyHead.position.y - (rock.position.y + rock.bounds.height));
+        if (heightDifference > 0.25f) {
+            boolean hitRightEdge = bunnyHead.position.x > (rock.position.x + rock.bounds.width / 2.0f);
+            if (hitRightEdge) {
+                bunnyHead.position.x = rock.position.x + rock.bounds.width;
+            } else {
+                bunnyHead.position.x = rock.position.x - bunnyHead.bounds.width;
+            }
+            return;
+        }
+
+        switch (bunnyHead.jumpState) {
+            case GROUNDED:
+                break;
+            case FALLING:
+            case JUMP_FALLING:
+                bunnyHead.position.y = rock.position.y + bunnyHead.bounds.height + bunnyHead.origin.y;
+                bunnyHead.jumpState = BunnyHead.JUMP_STATE.GROUNDED;
+                break;
+            case JUMP_RISING:
+                bunnyHead.position.y = rock.position.y + bunnyHead.bounds.height + bunnyHead.origin.y;
+                break;
+        }
+    }
+
+    private void onCollisionBunnyWithGoldCoin(GoldCoin goldcoin) {
+        goldcoin.collected = true;
+        score += goldcoin.getScore();
+        Gdx.app.log(TAG, "Gold coin collected");
+    }
+
+    private void onCollisionBunnyWithFeather(Feather feather) {
+        feather.collected = true;
+        score += feather.getScore();
+        level.bunnyHead.setFeatherPowerup(true);
+        Gdx.app.log(TAG, "Feather collected");
     }
 
     private void handleDebugInput(float deltaTime) {
@@ -155,15 +206,18 @@ public class WorldController extends InputAdapter {
 //        if (Gdx.input.isKeyPressed(Keys.W)) moveSelectedSprite(0, sprMoveSpeed);
 //        if (Gdx.input.isKeyPressed(Keys.S)) moveSelectedSprite(0, -sprMoveSpeed);
 
-        /* Camera Controls (move)*/
-        float camMoveSpeed = 5 * deltaTime;
-        float camMoveSpeedAccelerationFactor = 5;
-        if (Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) camMoveSpeed *= camMoveSpeedAccelerationFactor;
-        if (Gdx.input.isKeyPressed(Keys.LEFT)) moveCamera(-camMoveSpeed, 0);
-        if (Gdx.input.isKeyPressed(Keys.RIGHT)) moveCamera(camMoveSpeed, 0);
-        if (Gdx.input.isKeyPressed(Keys.UP)) moveCamera(0, camMoveSpeed);
-        if (Gdx.input.isKeyPressed(Keys.DOWN)) moveCamera(0, -camMoveSpeed);
-        if (Gdx.input.isKeyPressed(Keys.BACKSPACE)) cameraHelper.setPosition(0, 0);
+        if (!cameraHelper.hasTarget(level.bunnyHead)) {
+            /* Camera Controls (move)*/
+            float camMoveSpeed = 5 * deltaTime;
+            float camMoveSpeedAccelerationFactor = 5;
+            if (Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) camMoveSpeed *= camMoveSpeedAccelerationFactor;
+            if (Gdx.input.isKeyPressed(Keys.LEFT)) moveCamera(-camMoveSpeed, 0);
+            if (Gdx.input.isKeyPressed(Keys.RIGHT)) moveCamera(camMoveSpeed, 0);
+            if (Gdx.input.isKeyPressed(Keys.UP)) moveCamera(0, camMoveSpeed);
+            if (Gdx.input.isKeyPressed(Keys.DOWN)) moveCamera(0, -camMoveSpeed);
+            if (Gdx.input.isKeyPressed(Keys.BACKSPACE)) cameraHelper.setPosition(0, 0);
+        }
+
         // Camera Controls (zoom)
         float camZoomSpeed = 1 * deltaTime;
         float camZoomSpeedAccelerationFactor = 5;
@@ -173,12 +227,64 @@ public class WorldController extends InputAdapter {
         if (Gdx.input.isKeyPressed(Keys.SLASH)) cameraHelper.setZoom(1);
     }
 
+    private void handleInputGame(float deltaTime) {
+        if (cameraHelper.hasTarget(level.bunnyHead)) {
+            // Player Movement
+            if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+                level.bunnyHead.velocity.x = -level.bunnyHead.terminalVelocity.x;
+            } else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+                level.bunnyHead.velocity.x = level.bunnyHead.terminalVelocity.x;
+            } else {
+                // Execute auto-forward movement on non-desktop platform
+                if (Gdx.app.getType() != Application.ApplicationType.Desktop) {
+                    level.bunnyHead.velocity.x = level.bunnyHead.terminalVelocity.x;
+                }
+            }
+
+            // Bunny Jump
+            if (Gdx.input.isTouched() || Gdx.input.isKeyPressed(Keys.SPACE))
+                level.bunnyHead.setJumping(true);
+            else
+                level.bunnyHead.setJumping(false);
+        }
+    }
+
     private void moveCamera(float x, float y) {
         x += cameraHelper.getPosition().x;
         y += cameraHelper.getPosition().y;
         cameraHelper.setPosition(x, y);
     }
 
+    @Override
+    public boolean keyUp(int keycode) {
+        // Reset game world
+        if (keycode == Keys.R) {
+            init();
+            Gdx.app.debug(TAG, "Game world resetted");
+        }
+        // Toggle camera follow
+        else if (keycode == Keys.ENTER) {
+            cameraHelper.setTarget(cameraHelper.hasTarget() ? null : level.bunnyHead);
+            Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
+        }
+
+        /*for test*/
+        // Select next sprite
+//        else if (keycode == Keys.SPACE) {
+//            selectedSprite = (selectedSprite + 1) % testSprites.length;
+//            // Update camera's target to follow the currently selected sprite
+//            if (cameraHelper.hasTarget()) {
+//                cameraHelper.setTarget(testSprites[selectedSprite]);
+//            }
+//            Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
+//        }
+        // Toggle camera follow
+//        else if (keycode == Keys.ENTER) {
+//            cameraHelper.setTarget(cameraHelper.hasTarget() ? null : testSprites[selectedSprite]);
+//            Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
+//        }
+        return false;
+    }
 
     /*for test*/
 //    private void moveSelectedSprite(float x, float y) {
