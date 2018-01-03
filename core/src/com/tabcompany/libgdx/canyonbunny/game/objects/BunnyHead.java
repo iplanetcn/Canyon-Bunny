e2ee700275429a7dfd7b16ffce18ee211b70ef76
tabcompany <info.tabcompany@gmail.com>
2018-01-04 00:48:02 +0330

    Adding a dust particle effect to the player character

--- core/src/com/tabcompany/libgdx/canyonbunny/game/objects/BunnyHead.java
+++ core/src/com/tabcompany/libgdx/canyonbunny/game/objects/BunnyHead.java
@@ -1,5 +1,7 @@
 package com.tabcompany.libgdx.canyonbunny.game.objects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.tabcompany.libgdx.canyonbunny.game.Assets;
@@ -14,7 +16,9 @@ public class BunnyHead extends AbstractGameObject {
     private final float JUMP_TIME_MIN = 0.1f;
     private final float JUMP_TIME_OFFSET_FLYING = JUMP_TIME_MAX - 0.018f;
 
-    public enum VIEW_DIRECTION {LEFT, RIGHT}
+    public enum VIEW_DIRECTION {
+        LEFT, RIGHT
+    }
 
     public enum JUMP_STATE {
         GROUNDED, FALLING, JUMP_RISING, JUMP_FALLING
@@ -23,40 +27,53 @@ public class BunnyHead extends AbstractGameObject {
     private TextureRegion regHead;
 
     public VIEW_DIRECTION viewDirection;
-    public float timeJumping;
+
     public JUMP_STATE jumpState;
+    public float timeJumping;
+
     public boolean hasFeatherPowerup;
     public float timeLeftFeatherPowerup;
 
-    public BunnyHead() {
+    public ParticleEffect dustParticles = new ParticleEffect();
+
+    public BunnyHead () {
         init();
     }
 
-    public void init() {
+    public void init () {
         dimension.set(1, 1);
+
         regHead = Assets.instance.bunny.head;
+
         // Center image on game object
         origin.set(dimension.x / 2, dimension.y / 2);
+
         // Bounding box for collision detection
         bounds.set(0, 0, dimension.x, dimension.y);
+
         // Set physics values
         terminalVelocity.set(3.0f, 4.0f);
         friction.set(12.0f, 0.0f);
         acceleration.set(0.0f, -25.0f);
+
         // View direction
         viewDirection = VIEW_DIRECTION.RIGHT;
+
         // Jump state
         jumpState = JUMP_STATE.FALLING;
         timeJumping = 0;
+
         // Power-ups
         hasFeatherPowerup = false;
         timeLeftFeatherPowerup = 0;
+
+        // Particles
+        dustParticles.load(Gdx.files.internal("particles/dust.pfx"), Gdx.files.internal("particles"));
     }
 
     @Override
-    public void update(float deltaTime) {
+    public void update (float deltaTime) {
         super.update(deltaTime);
-
         if (velocity.x != 0) {
             viewDirection = velocity.x < 0 ? VIEW_DIRECTION.LEFT : VIEW_DIRECTION.RIGHT;
         }
@@ -68,13 +85,18 @@ public class BunnyHead extends AbstractGameObject {
                 setFeatherPowerup(false);
             }
         }
+        dustParticles.update(deltaTime);
     }
 
     @Override
-    protected void updateMotionY(float deltaTime) {
+    protected void updateMotionY (float deltaTime) {
         switch (jumpState) {
             case GROUNDED:
                 jumpState = JUMP_STATE.FALLING;
+                if (velocity.x != 0) {
+                    dustParticles.setPosition(position.x + dimension.x / 2, position.y);
+                    dustParticles.start();
+                }
                 break;
             case JUMP_RISING:
                 // Keep track of jump time
@@ -97,17 +119,20 @@ public class BunnyHead extends AbstractGameObject {
                 }
         }
         if (jumpState != JUMP_STATE.GROUNDED) {
+            dustParticles.allowCompletion();
             super.updateMotionY(deltaTime);
         }
     }
 
     @Override
-    public void render(SpriteBatch batch) {
+    public void render (SpriteBatch batch) {
         TextureRegion reg = null;
 
+        // Draw Particles
+        dustParticles.draw(batch);
+
         // Apply Skin Color
-        batch.setColor(
-                CharacterSkin.values()[GamePreferences.instance.charSkin].getColor());
+        batch.setColor(CharacterSkin.values()[GamePreferences.instance.charSkin].getColor());
 
         // Set special color when game object has a feather power-up
         if (hasFeatherPowerup) {
@@ -116,16 +141,26 @@ public class BunnyHead extends AbstractGameObject {
 
         // Draw image
         reg = regHead;
-        batch.draw(reg.getTexture(), position.x, position.y, origin.x,
-                origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation,
-                reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(),
-                reg.getRegionHeight(), viewDirection == VIEW_DIRECTION.LEFT, false);
+        batch.draw(reg.getTexture(), position.x, position.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y,
+                rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(),
+                viewDirection == VIEW_DIRECTION.LEFT, false);
 
         // Reset color to white
         batch.setColor(1, 1, 1, 1);
     }
 
-    public void setJumping(boolean jumpKeyPressed) {
+    public void setFeatherPowerup (boolean pickedUp) {
+        hasFeatherPowerup = pickedUp;
+        if (pickedUp) {
+            timeLeftFeatherPowerup = Constants.ITEM_FEATHER_POWERUP_DURATION;
+        }
+    }
+
+    public boolean hasFeatherPowerup () {
+        return hasFeatherPowerup && timeLeftFeatherPowerup > 0;
+    }
+
+    public void setJumping (boolean jumpKeyPressed) {
         switch (jumpState) {
             case GROUNDED: // Character is standing on a platform
                 if (jumpKeyPressed) {
@@ -139,8 +174,7 @@ public class BunnyHead extends AbstractGameObject {
                     jumpState = JUMP_STATE.JUMP_FALLING;
                 }
                 break;
-            case FALLING: // Falling down
-
+            case FALLING:// Falling down
             case JUMP_FALLING: // Falling down after jump
                 if (jumpKeyPressed && hasFeatherPowerup) {
                     timeJumping = JUMP_TIME_OFFSET_FLYING;
@@ -150,14 +184,4 @@ public class BunnyHead extends AbstractGameObject {
         }
     }
 
-    public void setFeatherPowerup(boolean pickedUp) {
-        hasFeatherPowerup = pickedUp;
-        if (pickedUp) {
-            timeLeftFeatherPowerup = Constants.ITEM_FEATHER_POWERUP_DURATION;
-        }
-    }
-
-    public boolean hasFeatherPowerup() {
-        return hasFeatherPowerup && timeLeftFeatherPowerup > 0;
-    }
 }
