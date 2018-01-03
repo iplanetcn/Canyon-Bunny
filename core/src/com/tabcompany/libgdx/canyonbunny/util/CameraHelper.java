d8366f21cf17ad94589bb494257b641d9f5f9dc4
tabcompany <info.tabcompany@gmail.com>
2018-01-04 01:10:17 +0330

    Smoothing with linear interpolation (Lerp)

--- core/src/com/tabcompany/libgdx/canyonbunny/util/CameraHelper.java
+++ core/src/com/tabcompany/libgdx/canyonbunny/util/CameraHelper.java
@@ -11,11 +11,10 @@ public class CameraHelper {
 
     private final float MAX_ZOOM_IN = 0.25f;
     private final float MAX_ZOOM_OUT = 10.0f;
+    private final float FOLLOW_SPEED = 4.0f;
 
     private Vector2 position;
     private float zoom;
-
-    //private Sprite target;
 
     private AbstractGameObject target;
 
@@ -27,8 +26,7 @@ public class CameraHelper {
     public void update(float deltaTime) {
         if (!hasTarget()) return;
 
-        position.x = target.position.x + target.origin.x;
-        position.y = target.position.y + target.origin.y;
+        position.lerp(target.position, FOLLOW_SPEED * deltaTime);
 
         // Prevent camera from moving down too far
         position.y = Math.max(-1f, position.y);
@@ -65,18 +63,6 @@ public class CameraHelper {
     public float getZoom() {
         return zoom;
     }
-
-    /*for test*/
-//    public void setTarget(Sprite target) {
-//        this.target = target;
-//    }
-//
-//    public Sprite getTarget() {
-//        return target;
-//    }
-//    public boolean hasTarget(Sprite target) {
-//        return hasTarget() && this.target.equals(target);
-//    }
 
     public boolean hasTarget() {
         return target != null;
